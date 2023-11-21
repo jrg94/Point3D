46cb092efd56cca5b7c2635233eb0a0fe2533ded
jrg94 <jeremy.grifski@gmail.com>
2023-11-21 15:32:52 -0500

    Fully implemented secondary methods

--- src/components/geometry/point/Point3DSecondary.java
+++ src/components/geometry/point/Point3DSecondary.java
@@ -16,4 +16,41 @@ public abstract class Point3DSecondary implements Point3D {
         this.setZ(this.getZ() + p.getZ());
     }
 
+    @Override
+    public void move(double x, double y, double z) {
+        this.setX(x);
+        this.setY(y);
+        this.setZ(z);
+    }
+
+    @Override
+    public void move(Point3D p) {
+        this.setX(p.getX());
+        this.setY(p.getY());
+        this.setZ(p.getZ());
+    }
+
+    @Override
+    public double distance(Point3D p) {
+        double xDifference = p.getX() - this.getX();
+        double yDifference = p.getY() - this.getY();
+        double zDifference = p.getZ() - this.getZ();
+        double sum = xDifference * xDifference + yDifference * yDifference + zDifference * zDifference;
+        return Math.sqrt(sum);
+    }
+
+    @Override
+    public Point3D vectorTo(Point3D p) {
+        Point3D vector = this.newInstance();
+        vector.setX(this.getX() - p.getX());
+        vector.setY(this.getY() - p.getY());
+        vector.setZ(this.getZ() - p.getZ());
+        return vector;
+    }
+
+    @Override
+    public boolean isOrigin() {
+        return this.getX() == 0 && this.getY() == 0 && this.getZ() == 0;
+    }
+
 }
