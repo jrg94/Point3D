40dc7fdd9846507f29707e3002d9d0955654990e
jrg94 <jeremy.grifski@gmail.com>
2023-11-21 15:40:49 -0500

    Let VSCode add the unimplemented methods

---
+++ src/components/geometry/point/Point1.java
@@ -0,0 +1,59 @@
+package components.geometry.point;
+
+public class Point1 extends Point3DSecondary {
+
+    @Override
+    public double getX() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getX'");
+    }
+
+    @Override
+    public double getY() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getY'");
+    }
+
+    @Override
+    public double getZ() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getZ'");
+    }
+
+    @Override
+    public double setX(double x) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setX'");
+    }
+
+    @Override
+    public double setY(double y) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setY'");
+    }
+
+    @Override
+    public double setZ(double z) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setZ'");
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+    }
+
+    @Override
+    public Point3D newInstance() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'newInstance'");
+    }
+
+    @Override
+    public void transferFrom(Point3D arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'transferFrom'");
+    }
+    
+}
