bab5a72995850f7ace32e7056e6569c1a5405b55
jrg94 <jeremy.grifski@gmail.com>
2023-11-26 23:50:31 -0500

    Added two more tests

--- test/components/geometry/point/Point3DTest.java
+++ test/components/geometry/point/Point3DTest.java
@@ -15,4 +15,22 @@ public class Point3DTest {
         assertEquals(0, p.getZ(), .0001);
     }
 
+    @Test 
+    public void testCoordinateConstructorWithDefaults() {
+        Point3D p = new Point3D1(0, 0, 0);
+
+        assertEquals(0, p.getX(), .0001);
+        assertEquals(0, p.getY(), .0001);
+        assertEquals(0, p.getZ(), .0001);
+    }
+
+    @Test 
+    public void testCoordinateConstructorWithoutDefaults() {
+        Point3D p = new Point3D1(5, 10, 3);
+
+        assertEquals(5, p.getX(), .0001);
+        assertEquals(10, p.getY(), .0001);
+        assertEquals(3, p.getZ(), .0001);
+    }
+
 }
